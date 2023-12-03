669170bec53787608e06fb08739edf2d168d688e
Moisés Freire <moisesflopesf@gmail.com>
2023-12-03 05:30:26 -0300

    JSON

---
+++ JSON.js
@@ -0,0 +1,16 @@
+let match = {
+    team1 : "Santa Cruz",
+    team2 : "Real Madrid",
+    playersTeam1: {
+        0: ["Grafite", 23],
+        1: ["Caça Rato", 11],
+        2: ["Tará", 10]
+    },
+    playersTeam2 : {
+        0: ["Vini Jr", 7],
+        1: ["Rodrygo", 11],
+        2: ["Bellingham", 5]
+     }
+
+
+}
